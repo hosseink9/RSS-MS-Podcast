5ea9ef64e7522d578b81c14769a049f96f82c236
hosseink9 <hosseinkalantari.29@gmail.com>
2023-11-04 15:13:38 +0330

    Add a endpoint for see all episodes

--- app/api/api_v1/endpoints/routers.py
+++ app/api/api_v1/endpoints/routers.py
@@ -1,6 +1,6 @@
 from fastapi.routing import APIRouter
 from fastapi import Depends, HTTPException, status
-from ....core.config import PODCASTLIST_ENDPOINT
+from ....core.config import EPISODELIST_ENDPOINT, PODCASTLIST_ENDPOINT
 import httpx
 
 router = APIRouter(tags=["podcast"])
@@ -10,4 +10,11 @@ router = APIRouter(tags=["podcast"])
 async def podcastlist():
     async with httpx.AsyncClient() as client:
         response = await client.get(PODCASTLIST_ENDPOINT)
+    return response.json()
+
+
+@router.get("/episodelist", status_code=status.HTTP_200_OK)
+async def podcastlist():
+    async with httpx.AsyncClient() as client:
+        response = await client.get(EPISODELIST_ENDPOINT)
     return response.json()
