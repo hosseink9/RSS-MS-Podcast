bb4bb697385c17ac53f17c9291da554ad85f6fbf
hosseink9 <hosseinkalantari.29@gmail.com>
2023-11-09 13:21:52 +0330

    Add logging setup

--- app/core/config.py
+++ app/core/config.py
@@ -1,2 +1,28 @@
 PODCASTLIST_ENDPOINT = 'http://127.0.0.1:8000/podcast/podcasts/'
 EPISODELIST_ENDPOINT = 'http://127.0.0.1:8000/episode/episodes/'
+JWT_SECRET_KEY = '693364d5ccaf48dd5ad582fef7bd190591401694551819386b62ee9bd41ccf01'
+ALGORITHM = "HS256"
+
+logging_config = {
+    "version": 1,
+    "formatters": {
+        "custom-json": {
+            "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
+            "format": "%(asctime)s %(process)s %(levelname)s %(name)s %(module)s %(funcName)s %(lineno)s"
+        }
+    },
+    "handlers": {
+        "elastic-search": {
+            "level": "INFO",
+            "class": "app.log.elastic_handler.ElasticHandler",
+            "formatter": "custom-json",
+        },
+    },
+    "loggers": {
+        "elastic-logger": {
+            "level": "INFO",
+            "handlers": ["elastic-search"],
+            "propagate": True
+        },
+    },
+}
